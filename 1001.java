d5df13bc1c90bb71e773c0ada379f59d474c5f78
Abu Zaffor Mohammad Jakaria <jakaria2768@diu.edu.bd>
2016-08-01 12:51:02 +0600

    Create 1001.java

---
+++ 1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+        Scanner add = new Scanner (System.in);
+    	int a,b;
+    	a=add.nextInt();
+    	b=add.nextInt();
+    	System.out.println("X = "+(a+b));
+    	
+    }
+  
+}
