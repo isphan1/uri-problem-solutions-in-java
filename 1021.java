1222ce62c56383a89fb9bb370a9a28263e21bcd9
Abu Zaffor Mohammad Jakaria <jakaria2768@diu.edu.bd>
2016-08-01 13:00:05 +0600

    Create 1021.java

---
+++ 1021.java
@@ -0,0 +1,57 @@
+import java.util.Scanner; 
+public class Main {
+static Scanner ab = new Scanner(System.in);
+	public static void main(String[] args) {
+	 
+		 Double b = ab.nextDouble();
+		int n;
+		n=(int) (b/100);
+		
+		System.out.println("NOATS:");
+		System.out.println(n+" nota(s) de R$ 100.00");
+		b%=100;
+		
+		n=(int) (b/50);
+		System.out.println(n+" nota(s) de R$ 50.00");
+		b%=50;
+		
+		n=(int) (b/20);
+		System.out.println(n+" nota(s) de R$ 20.00");
+		b%=20;
+		
+		n=(int) (b/10);
+		System.out.println(n+" nota(s) de R$ 10.00");
+		b%=10;
+		
+		n=(int) (b/5);
+		System.out.println(n+" nota(s) de R$ 5.00");
+		b%=5;
+		
+		n=(int) (b/2);
+		System.out.println(n+" nota(s) de R$ 2.00");
+		b%=2;
+		System.out.println("MOEDAS:");
+		n=(int) (b/1);
+		System.out.println(n+" moeda(s) de R$ 1.00");
+		b%=1;
+		
+		n=(int) (b/.50);
+		System.out.println(n+" moeda(s) de R$ 0.50");
+		b%=.50;
+		
+		n=(int) (b/.20);
+		System.out.println(n+" moeda(s) de R$ 0.20");
+		b%=.20;
+		
+		n=(int) (b/.10);
+		System.out.println(n+" moeda(s) de R$ 0.10");
+		b%=.10;
+		
+		n=(int) (b/.05);
+		System.out.println(n+" moeda(s) de R$ 0.05");
+		b%=.05;
+		
+		n=(int) (b/.01);
+		System.out.println(n+" moeda(s) de R$ 0.01");
+		}
+	}
