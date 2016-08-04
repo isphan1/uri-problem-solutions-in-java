481fbac0ba5b7c9d9e270876c10340607221e1b0
Abu Zaffor Mohammad Jakaria <jakaria2768@diu.edu.bd>
2016-08-04 11:14:22 +0600

    Create 1097.java

---
+++ 1097.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+	static Scanner ab = new Scanner(System.in);
+	public static void main(String[] args) {
+int i,j=1,k=4;
+while(j<10){
+		for(i=3;i>0;i--){
+	System.out.println("I="+j+" "+"J="+(k+i));
+	
+}k+=2;
+		j+=2;
+		
+}
+	}
+}
